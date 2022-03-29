344993f5385bd8b07ee895b5ae8188dbf372d2e4
cmenasse <cmenasse@student.42.fr>
2022-03-29 16:04:43 +0200

    js done

---
+++ algoplay copy/models/User.model.js
@@ -0,0 +1,14 @@
+const {Schema, model} = require("mongoose");
+
+const userSchema = new Schema(
+  {
+    username: {type: String, unique: true},
+    email: {type: String, unique: true},
+    password: {type: String},
+    isAdmin: {type: Boolean, default: false}
+  },
+  {timestamps: true}
+);
+
+const User = model("User", userSchema);
+module.exports = User;
