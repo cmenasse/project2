7fe375528b8374ae71ab9f3d40bdeb01ce0251b0
Léo Cheffi <contact@leocheffi.com>
2022-03-31 12:13:34 +0200

    fixed page with porfile page and puzzles page

--- algoplay/models/User.model.js
+++ algoplay/models/User.model.js
@@ -5,6 +5,11 @@ const userSchema = new Schema(
     username: {type: String, unique: true},
     password: {type: String},
     isAdmin: {type: Boolean, default: false},
+    firstname: {type: String},
+    lastname: {type: String},
+    age: {type: Number},
+    solved: {type: Number},
+    created: {type: Number},
   },
   {timestamps: true}
 );
