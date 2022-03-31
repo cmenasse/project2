3fec4077435b18e862a0cc60a247844acb9f5a21
Léo Cheffi <contact@leocheffi.com>
2022-03-31 15:59:21 +0200

    edit page with form

--- algoplay/middleware/ifCurrentUser.js
+++ algoplay/middleware/ifCurrentUser.js
@@ -1,6 +1,7 @@
 module.exports = (req, res, next) => {
     // checks if the user is logged in when trying to access a specific page
     if (req.session.user.username !== req.params.username) {
+      console.log('USERNAMES DO NOT MATCH, REDIRECTING')
       return res.redirect("/");
     }
     // req.user = req.session.user;
