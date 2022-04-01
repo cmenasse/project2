c3ce085f4c3b8e98f972c3d0faa6851537ce08e6
Léo Cheffi <contact@leocheffi.com>
2022-04-01 10:09:50 +0200

    fixed bugs

--- algoplay/middleware/exposeUser.js
+++ algoplay/middleware/exposeUser.js
@@ -1,6 +1,8 @@
 module.exports = (req, res, next) => {
     if (req.session.user) {
        res.locals.connectedUser = req.session.user
+       res.locals.connectedUser.createdAt = new Date(Date.parse(res.locals.connectedUser.createdAt))
+       console.log("exposeUser", res.locals.connectedUser.createdAt, typeof res.locals.connectedUser.createdAt)
     }else{
         res.locals.connectedUser = undefined
     }
